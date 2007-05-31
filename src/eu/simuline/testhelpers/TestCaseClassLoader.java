80dabb7b0842febcded9ee2137065321efab32d3
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2007-05-31 23:33:23 +0000

    @new 	added profiler. 	- various improvements. 	- the essential thing is: added defaultExclusions 	  in a way that the profiler classes cannot be loaded.
    git-svn-id: file:///SVN_Archive/Software/trunk@1770 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/eu/simuline/testhelpers/TestCaseClassLoader.java
+++ src/eu/simuline/testhelpers/TestCaseClassLoader.java
@@ -37,24 +37,33 @@ public class TestCaseClassLoader extends ClassLoader {
      * fields.                                                              *
      * -------------------------------------------------------------------- */
 
+    // **** better done with instrumentation. 
     static boolean stop;// NOPMD
 
 
     /** name of excluded properties file */
-    static final String EXCLUDED_FILE= "excluded.properties";
+    static final String EXCLUDED_FILE = "excluded.properties";
 
     /** scanned class path */
     private JavaPath jPath;
 
-    /** excluded paths */
+    /**
+     * excluded paths 
+     * This is initialized by {@link #readExcludedPackages} 
+     * and used by {@link #isExcluded}. 
+     */
     private List<String> excluded;
 	 
-    /** default excluded paths */
-    private final String[] defaultExclusions= {
+    /** 
+     * Default excluded paths. 
+     * @see #isExcluded
+     */
+    private final String[] defaultExclusions = {
 	"junit.",
-	"org.junit.",
+	"org.",
 	"java.",
 	"javax.",
+	"com.",
 	"sun."
     };
 
@@ -92,6 +101,18 @@ public class TestCaseClassLoader extends ClassLoader {
 	return ClassLoader.getSystemResourceAsStream(name);
     } 
 
+    /**
+     * Returns whether the name with the given name 
+     * is excluded from being loaded. 
+     * 
+     *
+     * @param name 
+     *    the fully qualified name of a class as a <code>String</code>. 
+     * @return 
+     *    a <code>boolean</code> which shows whether <code>name</code> 
+     *    starts with one of the prefixes given by {@link #excluded}. 
+     *    In this case the corresponding class is not loaded. 
+     */
     public boolean isExcluded(String name) {
 	for (int i= 0; i < this.excluded.size(); i++) {
 	    if (name.startsWith(this.excluded.get(i))) {
@@ -122,7 +143,7 @@ public class TestCaseClassLoader extends ClassLoader {
 	    return cls;
 	}
 	//
-	// Delegate the loading of excluded classes to the
+	// Delegate the loading of excluded classes to the 
 	// standard class loader.
 	//
 	if (isExcluded(name)) {
@@ -130,7 +151,7 @@ public class TestCaseClassLoader extends ClassLoader {
 		cls = findSystemClass(name);
 		return cls;
 	    } catch (ClassNotFoundException e) {// NOPMD 
-		// keep searching
+		// keep searching **** although excluded. 
 	    }
 	}
 	byte[] data= lookupClassData(name);
@@ -167,9 +188,26 @@ public class TestCaseClassLoader extends ClassLoader {
 	}
     }
 	
+    /**
+     * Initializes {@link #excluded} using {@link #defaultExclusions} 
+     * and {@link #EXCLUDED_FILE}. 
+     * <ol>
+     * <li>
+     * First the entries of {@link #defaultExclusions} 
+     * are added to {@link #excluded}. 
+     * <li>
+     * Then {@link #EXCLUDED_FILE} is interpreted as filename 
+     * and the file is interpreted as Properties File with property keys 
+     * starting with <code>excluded.</code>; 
+     * all the other properties are ignored. 
+     * The values are trimmed 
+     * and a trailing <code>*</code> is removed if present. 
+     * If the remaining path is nontrivial, it is added to {@link #excluded}. 
+     * </ol>
+     */
     private void readExcludedPackages() {		
-	this.excluded= new ArrayList<String>(10);
-	for (int i= 0; i < defaultExclusions.length; i++) {
+	this.excluded = new ArrayList<String>(10);
+	for (int i= 0; i < this.defaultExclusions.length; i++) {
 	    this.excluded.add(defaultExclusions[i]);
 	}
 	InputStream inStream = getClass().getResourceAsStream(EXCLUDED_FILE);
@@ -192,9 +230,9 @@ public class TestCaseClassLoader extends ClassLoader {
 	    String key = (String)e.nextElement();
 	    if (key.startsWith("excluded.")) {
 		String path = prop.getProperty(key);
-		path= path.trim();
+		path = path.trim();
 		if (path.endsWith("*")) {
-		    path= path.substring(0, path.length()-1);
+		    path = path.substring(0, path.length()-1);
 		}
 		if (path.length() > 0) {
 		    this.excluded.add(path);
