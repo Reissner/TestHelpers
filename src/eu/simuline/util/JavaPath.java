940bfc8b5543ae6a561f5fe7bbc87a8a4b8005e7
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2007-06-01 02:06:11 +0000

    @new 	added profiler. 	- added GPL 	- replaced exception by warning.
    git-svn-id: file:///SVN_Archive/Software/trunk@1774 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/eu/simuline/util/JavaPath.java
+++ src/eu/simuline/util/JavaPath.java
@@ -1,3 +1,23 @@
+/*
+  Copyright (C) Simuline Inc, Ernst Rei3ner
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the 
+  Free Software Foundation, Inc., 
+  51 Franklin Street, Fifth Floor, 
+  Boston, MA  02110-1301, USA.
+*/
+
 package eu.simuline.util;
 
 import java.util.List;
@@ -260,8 +280,11 @@ public class JavaPath {
 	this.roots = new ArrayList<File>(fileNames.length);
 	for (int i = 0; i < fileNames.length; i++) {
 	    if (fileNames[i].length() == 0) {
-		throw new IllegalArgumentException
-		    ("Found file \"\" in path \"" + path + "\". ");
+		System.out.println
+("Warning: Found file \"\" in path \"" + path + "\". ");
+		
+// 		throw new IllegalArgumentException
+// 		    ("Found file \"\" in path \"" + path + "\". ");
 	    }
 	    this.roots.add(new File(fileNames[i]));
 	}
