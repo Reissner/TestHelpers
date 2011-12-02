cd58949d126edb0b5c8951f765ceb07eb368fbf8
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2011-12-02 22:06:19 +0000

    @doc 	added comment
    git-svn-id: file:///SVN_Archive/Software/trunk@4638 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/eu/simuline/testhelpers/Accessor.java
+++ src/eu/simuline/testhelpers/Accessor.java
@@ -1024,7 +1024,7 @@ public final class Accessor<T> {
 	
 	Constructor<T> toBeInvoked = 
 	    getConstructor(aClass,
-			   // **** this is a weakness in the jdk5 (reported)
+			   // **** the cast is a weakness in the jdk5 (reported)
 			   // **** in jdk6 hardly any improvement (to be rep.)
 			   (Constructor<T>[])aClass.getDeclaredConstructors(),
 			   parameters);
