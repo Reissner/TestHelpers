00860334e2602b59be88a86072b0888023d8a17e
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2013-01-03 09:41:22 +0000

    @new 	to implement find . -exec grep Octave {} ; 	- added Finder class ExecFilter 	- added method exec(...) @chg 	found that NameFilter is similar and that futher filters can be rewritten in the same style 	- rewritten classes Primary, NameFilter and PrintFilter 	- added class Secondary which may in future serve as superclass @new 	started adding boolean expressions 	- added classes NegFilter and Multiplexer with inner class Slave. 	- added method neg(...) 242-->567
    git-svn-id: file:///SVN_Archive/Software/trunk@5917 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/eu/simuline/util/Finder.java
+++ src/eu/simuline/util/Finder.java
@@ -3,7 +3,10 @@ package eu.simuline.util;
 import java.io.File;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.IOException;
 
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Arrays;
 import java.util.Stack;
 import java.util.regex.Pattern;
@@ -14,12 +17,12 @@ import java.util.regex.Pattern;
  * [-D help|tree|search|stat|rates|opt|exec] [path...] (expression)*</code>. 
  * If <code>path</code> is left out (possibly more than one path?), 
  * then the default is <code>.</code>, i.e. the current directory. 
- * This need not be a directory. 
+ * The <code>path</code> need not be a directory. 
  * If it is a proper file then typically wildcards are used. 
  * Then the name is matched. 
  * If no expression is given, the default is <code>-print</code>. 
  * So we may assume, 
- * that a path and a non-empty sequence of expressions is given. 
+ * that both, a path and a non-empty sequence of expressions, are given. 
  * <p>
  * The idea behind the <code>find</code> command is, 
  * that, starting with the files matching the <code>path</code>, 
@@ -30,7 +33,13 @@ import java.util.regex.Pattern;
  * For tests, the focus is on the filter-functionality, 
  * whereas actions are trivial filters just passing the files they receive. 
  * Actions are applied because of their side effects like <code>print</code>. 
- *
+ * <p>
+ * The most basic kind of finder corresponds with the command 
+ * <code>find path</code> iterating just over the files in the path. 
+ * This kind of finder is returned by the static method {@link #path(File)}. 
+ * Starting with this basic finder, further finders can be added to the pipe 
+ * using the member methods 
+ * {@link #name(String)} and {@link # print(PrintStream)}. 
  *
  * Created: Wed Nov 21 17:29:41 2012
  *
@@ -50,7 +59,7 @@ public abstract class Finder {
 	 * fields.                                                          *
 	 * ---------------------------------------------------------------- */
 
-	private Stack<File> files;
+	private final Stack<File> files;
 
 	/* ---------------------------------------------------------------- *
 	 * constructors.                                                    *
@@ -87,10 +96,81 @@ public abstract class Finder {
 	    }
 	    return file;
 	}
+
+	public boolean pass(File file) {
+	    throw new UnsupportedOperationException();
+	}
+
     } // class Primary 
 
+    class Multiplexer {
+
+	/* ---------------------------------------------------------------- *
+	 * inner classes.                                                   *
+	 * ---------------------------------------------------------------- */
+
+	class Slave extends Finder {
+	    public boolean hasNext() {
+		if (Multiplexer.this.read.contains(Slave.this)) {
+		    return Finder.this.hasNext();
+		}
+		return Multiplexer.this.file != null;
+	    }
+
+	    public File next() {
+		if (Multiplexer.this.read.contains(Slave.this)) {
+		    assert read.size() == slaves.size();// **** 
+		    Multiplexer.this.read.clear();
+		    Multiplexer.this.file = Finder.this.next();
+		}
+		Multiplexer.this.read.add(Slave.this);
+
+		return Multiplexer.this.file;
+	    }
+
+	    public boolean pass(File file) {
+		throw new UnsupportedOperationException();
+	    }
+
+	} // class Slave 
+
+	/* ---------------------------------------------------------------- *
+	 * fields.                                                          *
+	 * ---------------------------------------------------------------- */
+
+	private final Set<Finder> slaves;
+	private final Set<Finder> read;
+	private File file;
+
+	/* ---------------------------------------------------------------- *
+	 * constructors.                                                    *
+	 * ---------------------------------------------------------------- */
+
+	Multiplexer() {
+	    this.slaves = new HashSet<Finder>();
+	    this.read   = new HashSet<Finder>();
+	    if (Finder.this.hasNext()) {
+		this.file = Finder.this.next();
+	    } else {
+		this.file = null;
+	    }
+	}
+
+	/* ---------------------------------------------------------------- *
+	 * methods.                                                         *
+	 * ---------------------------------------------------------------- */
+
+	Finder finder() {
+	    Finder res = new Slave();
+	    this.slaves.add(res);
+	    return res;
+	}
+
+
+    } // class Multiplexer 
+
     /**
-     * Filters by name. See {@link Finder#name(String)}. 
+     * Filters files by name. See {@link Finder#name(String)}. 
      */
     class NameFilter extends Finder {
 
@@ -98,8 +178,17 @@ public abstract class Finder {
 	 * fields.                                                          *
 	 * ---------------------------------------------------------------- */
 
-	private Pattern pattern;
-	private File next;
+	/**
+	 * The pattern to filter the filenames with. 
+	 */
+	private final Pattern pattern;
+
+	/**
+	 * The next file to be returned by {@link #next()} if any, 
+	 * i.e. if  {@link #hasNext()} returns <code>true</code>; 
+	 * otherwise this field is <code>null</code>. 
+	 */
+	protected File next;
 
 	/* ---------------------------------------------------------------- *
 	 * constructors.                                                    *
@@ -114,17 +203,19 @@ public abstract class Finder {
 	 * methods.                                                         *
 	 * ---------------------------------------------------------------- */
 
-	private void updateNext() {
-	    boolean matches;
+	protected void updateNext() {
 	    while (Finder.this.hasNext()) {
 		this.next = Finder.this.next();
 		assert this.next != null;
-		matches = this.pattern.matcher(this.next.getName()).matches();
-		if (matches) {
+		if (pass(this.next)) {
 		    return;
 		}
 	    }
 	    this.next = null;
+	}
+
+	public boolean pass(File file) {
+	    return this.pattern.matcher(file.getName()).matches();
 	}
 
 	public boolean hasNext() {
@@ -137,9 +228,30 @@ public abstract class Finder {
 	    assert res != null;
 	    updateNext();
 	    return res;
-
 	}
     } // class NameFilter 
+
+    abstract class Secondary extends Finder {
+
+	/* ---------------------------------------------------------------- *
+	 * fields.                                                          *
+	 * ---------------------------------------------------------------- */
+
+	/**
+	 * The next file to be returned by {@link #next()} if any, 
+	 * i.e. if  {@link #hasNext()} returns <code>true</code>; 
+	 * otherwise this field is <code>null</code>. 
+	 */
+	protected File next;
+
+	/* ---------------------------------------------------------------- *
+	 * constructors.                                                    *
+	 * ---------------------------------------------------------------- */
+
+	Secondary() {
+	}
+
+    } // class Secondary 
 
     class PrintFilter extends Finder {
 
@@ -147,7 +259,9 @@ public abstract class Finder {
 	 * fields.                                                          *
 	 * ---------------------------------------------------------------- */
 
-	private PrintStream str;
+	private final PrintStream str;
+
+	protected File next;
 
 	/* ---------------------------------------------------------------- *
 	 * constructors.                                                    *
@@ -155,23 +269,204 @@ public abstract class Finder {
 
 	PrintFilter(PrintStream str) {
 	    this.str = str;
+	    updateNext();
 	}
 
 	/* ---------------------------------------------------------------- *
 	 * methods.                                                         *
 	 * ---------------------------------------------------------------- */
 
+	protected void updateNext() {
+	    while (Finder.this.hasNext()) {
+		this.next = Finder.this.next();
+		assert this.next != null;
+		if (pass(this.next)) {
+		    return;
+		}
+	    }
+	    this.next = null;
+	}
+
+	public boolean pass(File file) {
+	    return true;
+	}
+
+	// public boolean hasNext() {
+	//     return Finder.this.hasNext();
+	// }
+
 	public boolean hasNext() {
-	    return Finder.this.hasNext();
+	    return this.next != null;
+	}
+
+	// public File next() {
+	//     assert hasNext();
+	//     File res = Finder.this.next();
+	//     this.str.println(res.toString());
+	//     return res;
+	// }
+
+	public File next() {
+	    assert hasNext();
+	    File res = this.next;
+	    assert res != null;
+	    this.str.println(res.toString());
+	    updateNext();
+	    return res;
+	}
+    } // class PrintFilter 
+
+    class ExecFilter extends Finder {
+
+	/* ---------------------------------------------------------------- *
+	 * fields.                                                          *
+	 * ---------------------------------------------------------------- */
+
+	/**
+	 * The command to be executed including arguments 
+	 * separated by space as to be passed to {@link Runtime#exec(String)}. 
+	 */
+	private final String[] cmd;
+
+	/**
+	 * The next file to be returned by {@link #next()} if any, 
+	 * i.e. if  {@link #hasNext()} returns <code>true</code>; 
+	 * otherwise this field is <code>null</code>. 
+	 */
+	protected File next;
+
+	/* ---------------------------------------------------------------- *
+	 * constructors.                                                    *
+	 * ---------------------------------------------------------------- */
+
+	ExecFilter(String[] cmd) {
+	    this.cmd = cmd;
+	    updateNext();
+	}
+
+	/* ---------------------------------------------------------------- *
+	 * methods.                                                         *
+	 * ---------------------------------------------------------------- */
+
+	protected void updateNext() {
+	    while (Finder.this.hasNext()) {
+		this.next = Finder.this.next();
+		assert this.next != null;
+		if (pass(this.next)) {
+		    return;
+		}
+	    }
+	    this.next = null;
+	}
+
+	public boolean pass(File file) {
+	    Process proc;
+	    int exitVal;
+	    String nextStr = file.toString();
+
+	    // replace argument by filename 
+	    String[] cmd = new String[this.cmd.length];
+	    System.arraycopy(this.cmd, 0, cmd, 0, this.cmd.length);
+	    for (int idx=0; idx < this.cmd.length; idx++) {
+		if (cmd[idx] == EXEC_ARG) {
+		    cmd[idx] = nextStr;
+		}
+	    }
+
+	    try {
+		//System.out.println(":"+(this.cmdName+" "+this.next));
+		proc = Runtime.getRuntime().exec(cmd);
+	    } catch (IOException e) {
+		System.out.println("exec: "+e.getMessage());
+		return false;
+	    }
+	    try {
+		exitVal = proc.waitFor();// exitValue
+	    } catch (InterruptedException e) {
+		System.out.println("exec:"+e.getMessage());
+		return false;
+	    }
+	    return exitVal == 0;
+	}
+
+	public boolean hasNext() {
+	    return this.next != null;
 	}
 
 	public File next() {
 	    assert hasNext();
-	    File res = Finder.this.next();
-	    this.str.println(res.toString());
+	    File res = this.next;
+	    assert res != null;
+	    updateNext();
 	    return res;
 	}
-    } // class PrintFilter 
+    } // class ExecFilter 
+
+    class NegFilter extends Finder {
+
+	/* ---------------------------------------------------------------- *
+	 * fields.                                                          *
+	 * ---------------------------------------------------------------- */
+
+	private final Finder negFilter;
+
+	protected File next;
+
+	/* ---------------------------------------------------------------- *
+	 * constructors.                                                    *
+	 * ---------------------------------------------------------------- */
+
+	NegFilter(Finder negFilter) {
+	    this.negFilter = negFilter;
+	    updateNext();
+	}
+
+	/* ---------------------------------------------------------------- *
+	 * methods.                                                         *
+	 * ---------------------------------------------------------------- */
+
+	protected void updateNext() {
+	    File notNext;
+	    while (Finder.this.hasNext()) {
+		this.next = Finder.this.next();
+		assert this.next != null;
+		if (pass(this.next)) {
+		    return;
+		}
+	    }
+	    this.next = null;
+	}
+
+	public boolean pass(File file) {
+	    return !this.negFilter.pass(file);
+	}
+
+	public boolean hasNext() {
+	    return this.next != null;
+	}
+
+	public File next() {
+	    assert hasNext();
+	    File res = this.next;
+	    assert res != null;
+	    updateNext();
+	    return res;
+	}
+	// public File next() {
+	//     if (Finder.this.hasNext()) {
+
+	//     }
+	//     return null;
+	// }
+
+
+    } // class NegFilter 
+
+    /* -------------------------------------------------------------------- *
+     * Class constants.                                                     *
+     * -------------------------------------------------------------------- */
+
+    public final static String EXEC_ARG = "{}";
 
     /* -------------------------------------------------------------------- *
      * constructors and creator methods.                                    *
@@ -192,6 +487,10 @@ public abstract class Finder {
 	return new Finder.Primary(file);
     }
 
+    Multiplexer multiplexer() {
+	return new Multiplexer();
+    }
+
     /**
      * Filter without side effect: 
      * just passes the files received by this finder 
@@ -210,6 +509,15 @@ public abstract class Finder {
 	return new PrintFilter(str);
     }
 
+    public Finder exec(String[] cmd) {
+	return new ExecFilter(cmd);
+    }
+
+    public Finder neg(Finder filter) {
+	return new NegFilter(filter);
+    }
+
+
     /* -------------------------------------------------------------------- *
      * abstract methods.                                                    *
      * -------------------------------------------------------------------- */
@@ -227,14 +535,31 @@ public abstract class Finder {
      */
     public abstract File next();
 
-    public static void main(String[] args) {
-	File file = new File(args[0]);
-	Finder finder = Finder.path(file)
-	    //.name(".*\\.java")
-	    .print(System.out);
-	while (finder.hasNext()) {
-	    System.out.println(""+finder.next());
+    public abstract boolean pass(File file);
 
+    public static void main(String[] args) throws Exception {
+	//	int res = Runtime.getRuntime()
+//	    .exec(new String[] {"grep", "Octave", "./src/eu/simuline/matlab/Matlab.g"})
+//	    .waitFor() ;
+//	System.out.println("res: "+res);
+//	System.exit(0);
+
+	File file = new File(args[0]);
+
+	Finder finder = Finder.path(file)
+	    .name(".*\\.java")
+	    .exec(new String[] {"grep", "matlab", EXEC_ARG})
+	    .print(System.out);
+
+	// Finder finder = Finder.path(file);
+	// Multiplexer mult = finder.multiplexer();
+	// Finder negFilter = mult.finder()
+	//     .exec(new String[] {"grep", "'", EXEC_ARG});
+	// finder = mult.finder().neg(negFilter);
+
+	while (finder.hasNext()) {
+	    finder.next();
+//	    System.out.println(""+finder.next());
 	}
     }
 
