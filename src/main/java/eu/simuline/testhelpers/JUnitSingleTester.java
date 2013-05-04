f9bbf35a9ff79b51ba269c261957b578ae9c32d1
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2013-05-04 19:40:57 +0000

    @new 	allows to run all tests with java-gui's at once. 	serves e.g to check whether this is possible with the given settings.
    git-svn-id: file:///SVN_Archive/Software/trunk@6333 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/main/java/eu/simuline/testhelpers/JUnitSingleTester.java
@@ -0,0 +1,60 @@
+
+package eu.simuline.testhelpers;
+
+import eu.simuline.util.Finder;
+
+import java.io.File;
+
+public class JUnitSingleTester {
+
+    static class RunnerThread extends Thread {
+	Class tstCls;
+	RunnerThread(Class tstCls) {
+	    this.tstCls = tstCls;
+	}
+
+	public void run() {
+	    Actions.run(tstCls);
+	}
+    } // class RunnerThread 
+
+    /**
+     * Runs all testclasses in the directory given by the 0th argument 
+     * in the package **** given by the second argument. 
+     */
+    public static void main(String[] args) throws Exception {
+	File baseDir = new File(args[0]+args[1]);
+	String dirName = baseDir.getAbsolutePath();
+	Finder finder = Finder.path(baseDir)
+	    .name(".*arithmetics.*")
+//	    .not(Finder.nameFilter(".*arithmetics.*"))
+	    .not(Finder.nameFilter(".*graphDV.*"))
+	    .name(".*Test\\.class")
+//	    .print(System.out)
+	    ;
+
+	File clsFile;
+	String clsName;
+	Class tstCls;
+
+	while (finder.hasNext()) {
+	    clsFile = finder.next();
+	    clsName = clsFile.getAbsolutePath();
+System.out.println("clsName"+clsName);
+
+	    assert clsName.startsWith(dirName);
+	    clsName = clsName.substring(args[0].length());
+	    clsName = clsName.substring(0, clsName.length()-".class".length());
+System.out.println("clsName"+clsName);
+
+	    clsName = clsName.replace(System.getProperty("file.separator")
+				      .charAt(0),'.');
+	    System.out.println("clsName: "+clsName);
+	    tstCls = Class.forName(clsName);
+	    //Actions.run(tstCls);
+	    new RunnerThread(tstCls).start();
+
+	}
+
+    }
+} // class JUnitSingleTester 
