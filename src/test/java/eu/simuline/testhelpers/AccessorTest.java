3172605bdd6068dc1f7aa1b817f7a33d18a06014
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2013-05-10 01:09:50 +0000

    @chg 	updated main methods to work with "new" framework.
    git-svn-id: file:///SVN_Archive/Software/trunk@6352 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/test/java/eu/simuline/testhelpers/AccessorTest.java
+++ src/test/java/eu/simuline/testhelpers/AccessorTest.java
@@ -1,7 +1,7 @@
 
 package eu.simuline.testhelpers;
 
-import eu.simuline.testhelpers.GUIRunListener;
+import eu.simuline.testhelpers.Actions;
 //import eu.simuline.testhelpers.Accessor;
 //import static eu.simuline.testhelpers.Assert;
 
@@ -585,14 +585,7 @@ public class AccessorTest {
      * Uncomment either the textual UI, Swing UI, or AWT UI.
      */
     public static void main(String args[]) {
-/*
-	JUnitCore core = new JUnitCore();
-	core.addListener(new GUIRunListener());
-	core.run(AccessorTest.class);
-*/
-
 	Actions.run(AccessorTest.class);
-
     }
 
 }
