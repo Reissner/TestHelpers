de9105cc4a081af9d7875ff9f5b838be630ac2e9
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2014-10-30 14:18:27 +0000

    @new 	preparation of methods FNNumber.cond, distr: 	- added methods known from SortedSet, in particular first() and last(). 1423-->1589
    git-svn-id: file:///SVN_Archive/Software/trunk@8024 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/eu/simuline/util/MultiSet.java
+++ src/main/java/eu/simuline/util/MultiSet.java
@@ -11,7 +11,7 @@ import java.util.Comparator;
 import java.util.Collections;
 
 /**
- * Represents a set with multiplicities. 
+ * Represents a sorted set with multiplicities. 
  * Mathematically this is something between a set and a family. 
  * Note that this kind of set does not support <code>null</code> elements. 
  * <p>
@@ -19,9 +19,12 @@ import java.util.Collections;
  * either from a set or as a copy of another <code>MultiSet</code>. 
  * <p>
  * Note that this should implement Collection, but still does not *****. 
+ * maybe it should even implement Set or SortedSet. 
  * addAll's implementation seems strange, 
  * add seems to be buggy, 
  * One should not require a TreeMap alternative: HashMap. 
+ * So one should distinguish SortedMultiSet and MultiSet 
+ * and both should be interfaces. 
  * Problem with overflow of multiplicities. 
  *
  * @author <a href="mailto:ereissner@rig35.rose.de">Ernst Reissner</a>
@@ -807,6 +810,169 @@ public class MultiSet<T> implements Iterable<T> {
 	// throws NullPointerException for obj==null 
 	return getMultiplicityObj(obj) != null;
     }
+
+    /**
+     * Returns the comparator used to order the elements in this set, 
+     * or <code>null</code> 
+     * if this set uses the natural ordering of its elements. 
+     *
+     * @return
+     *    the comparator used to order the elements in this set, 
+     *    or <code>null</code> 
+     *    if this set uses the natural ordering of its elements. 
+     */
+    Comparator<? super T> comparator() {
+	return this.obj2mult.comparator();
+    }
+
+    /**
+     * Returns the first (lowest) element currently in this set.
+     *
+     * @return
+     *    the first (lowest) element currently in this set
+     * @throws NoSuchElementException
+     *    if this set is empty. 
+     */
+    public T first() {
+	return this.obj2mult.firstKey();
+    }
+
+    /**
+     * Returns the last (highest) element currently in this set.
+     *
+     * @return
+     *    the last (highest) element currently in this set. 
+     * @throws NoSuchElementException
+     *    if this set is empty. 
+     */
+    public T last() {
+	return this.obj2mult.lastKey();
+    }
+
+    /**
+     * Returns a view of the portion of this multi-set 
+     * whose elements are strictly less than <code>toElement</code>. 
+     * The returned multi-set is backed by this multi-set, 
+     * so changes in the returned set are reflected in this multi-set, 
+     * and vice-versa. 
+     * The returned multi-set supports all optional multi-set operations 
+     * that this multi-set supports.
+     * <p>
+     * The returned multi-set 
+     * will throw an <code>IllegalArgumentException</code> 
+     * on an attempt to insert an element outside its range. 
+     *
+     * @param toElement
+     *    high endpoint (exclusive) of the returned multi-set. 
+     * @return
+     *    a view of the portion of this multi-set 
+     *    whose elements are strictly less than <code>toElement</code>. 
+     * @throws ClassCastException
+     *    if <code>toElement</code> is not compatible 
+     *    with this multi-set's comparator 
+     *    (or, if the set has no comparator, 
+     *    if <code>toElement</code> does not implement {@link Comparable}). 
+     *    Implementations may, but are not required to, 
+     *    throw this exception if <code>toElement</code> cannot be compared 
+     *    to elements currently in this multi-set. 
+     * @throws NullPointerException
+     *    if <code>toElement</code> is <code>null</code> 
+     *    and this multi-set does not permit <code>null</code> elements. 
+     * @throws IllegalArgumentException
+     *    if this multi-set itself has a restricted range, 
+     *    and <code>toElement</code> lies outside the bounds of the range. 
+     */
+    public MultiSet<T> headSet(T toElement) {
+	return new MultiSet<T>(this.obj2mult.headMap(toElement));
+    }
+
+    /**
+     * Returns a view of the portion of this multi-set 
+     * whose elements are greater than or equal to <code>fromElement</code>. 
+     * The returned multi-set is backed by this multi-set, 
+     * so changes in the returned set are reflected in this multi-set, 
+     * and vice-versa. 
+     * The returned multi-set supports all optional multi-set operations 
+     * that this multi-set supports.
+     * <p>
+     * The returned multi-set 
+     * will throw an <code>IllegalArgumentException</code> 
+     * on an attempt to insert an element outside its range. 
+     *
+     * @param fromElement
+     *    low endpoint (inclusive) of the returned multi-set. 
+     * @return
+     *    a view of the portion of this multi-set 
+     *    whose elements are greater than or equal to <code>fromElement</code>. 
+     * @throws ClassCastException
+     *    if <code>fromElement</code> is not compatible 
+     *    with this multi-set's comparator 
+     *    (or, if the set has no comparator, 
+     *    if <code>fromElement</code> does not implement {@link Comparable}). 
+     *    Implementations may, but are not required to, 
+     *    throw this exception if <code>fromElement</code> cannot be compared 
+     *    to elements currently in this multi-set. 
+     * @throws NullPointerException
+     *    if <code>fromElement</code> is <code>null</code> 
+     *    and this multi-set does not permit <code>null</code> elements. 
+     * @throws IllegalArgumentException
+     *    if this multi-set itself has a restricted range, 
+     *    and <code>fromElement</code> lies outside the bounds of the range. 
+     */
+    public MultiSet<T> tailSet(T fromElement) {
+	return new MultiSet<T>(this.obj2mult.tailMap(fromElement));
+    }
+
+    /**
+     * Returns a view of the portion of this multi-set 
+     * whose elements range from <code>fromElement</code> inclusively 
+     * to <code>toElement</code> exclusively. 
+     * The returned multi-set is backed by this multi-set, 
+     * so changes in the returned set are reflected in this multi-set, 
+     * and vice-versa. 
+     * The returned multi-set supports all optional multi-set operations 
+     * that this multi-set supports.
+     * <p>
+     * The returned multi-set 
+     * will throw an <code>IllegalArgumentException</code> 
+     * on an attempt to insert an element outside its range. 
+     *
+     * @param fromElement
+     *    low endpoint (inclusive) of the returned multi-set. 
+     * @param toElement
+     *    high endpoint (exclusive) of the returned multi-set. 
+     * @return
+     *    a view of the portion of this multi-set 
+     *    from <code>fromElement</code> inclusively 
+     *    to <code>toElement</code> exclusively. 
+     * @throws ClassCastException **** maybe original documentation wrong. **** 
+     *    if <code>fromElement</code> and <code>toElement</code> 
+     *    cannot be compared to one another using this set's comparator 
+     *    (or, if the set has no comparator, using natural ordering). 
+     *    or if <code>fromElement</code> is not compatible 
+     *    with this multi-set's comparator 
+     *    (or, if the set has no comparator, 
+     *    if <code>fromElement</code> does not implement {@link Comparable}). 
+     *    Implementations may, but are not required to, 
+     *    throw this exception 
+     *    if <code>fromElement</code> or <code>toElement</code> 
+     *    cannot be compared to elements currently in this multi-set. 
+     * @throws NullPointerException
+     *    if <code>fromElement</code> or <code>toElement</code> 
+     *    is <code>null</code> 
+     *    and this multi-set does not permit <code>null</code> elements. 
+     * @throws IllegalArgumentException
+     *    if <code>fromElement</code> is greater than <code>toElement</code> 
+     *    or if this multi-set itself has a restricted range, 
+     *    and <code>fromElement</code> or <code>toElement</code> 
+     *    lies outside the bounds of the range. 
+     */
+    public MultiSet<T> subSet(T fromElement, T toElement) {
+	return new MultiSet<T>(this.obj2mult.subMap(fromElement, toElement));
+    }
+
+
+
 
     /**
      * Returns an iterator over the elements in this collection 
