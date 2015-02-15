1a9cee273785baaf0191d2ac9afe1ba35c5aa829
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2015-02-15 12:34:52 +0000

    @doc
    git-svn-id: file:///SVN_Archive/Software/trunk@8324 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/eu/simuline/util/DataModel.java
+++ src/main/java/eu/simuline/util/DataModel.java
@@ -52,6 +52,17 @@ public enum DataModel {
      * methods                                                              *
      *----------------------------------------------------------------------*/
 
+    /**
+     * Returns the current data model. 
+     * If there is no valid data model, (not even {@link #Unknown}), 
+     * then <code>null</code> is returned if <code>retNull</code> is set. 
+     * Otherwise an exception is thrown. 
+     *
+     * @param retNull
+     *    whether <code>null</code> is returned if the data model is invalid. 
+     * @throws IllegalStateException
+     *    if the data model is unknown and <code>retNull</code> is not set. 
+     */
     public static DataModel getDataModel(boolean retNull) {
 	String code = System.getProperty("sun.arch.data.model");
 	DataModel dataModel = null;
@@ -71,6 +82,9 @@ public enum DataModel {
 	return this.code;
     }
 
+    /**
+     * Returns true except for {@link #Unknown}. 
+     */
     public boolean isKnown() {
 	return true;
     }
