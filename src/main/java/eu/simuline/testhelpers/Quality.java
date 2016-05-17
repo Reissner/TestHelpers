ec8e0ed4b850dad206d1c425144f1ffcb98cfe8a
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2016-05-17 10:48:22 +0000

    @new 	complete redesign: now 3 kinds of methods are supported: 	- setXXX for state XXX and time 	  added methods setAssumptionFailure() 	  added method  setFailure(org.junit.runner.notification.Failure) 	    moving functionality from TestCase to Quality 	  eliminated method setIgnored() because this is realized in Testcase via constructor. 	- isXXX, allowsReRun for property XXX andisIrregular( 	  added methods allowsRerun(), isIgnored(), @bugfix 	   eliminated method  isDecided() which is replaced by allowsRerun() 	- getXXX for representation XXXand time 	  - added methods time(long) moving functionality from TestCase to Quality 	  - getColor(), max() (and for that field level) to provide 3 colors in progress bar 	    added also static fields COLOR_XXX @doc 	state transitions 	with Junit 4.12: support AssumptionFailures: 	- added method setAssumptionFailure() 	- implemented Invalidated. getIcon() 237-->457
    git-svn-id: file:///SVN_Archive/Software/trunk@10364 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/eu/simuline/testhelpers/Quality.java
+++ src/main/java/eu/simuline/testhelpers/Quality.java
@@ -5,39 +5,74 @@ import eu.simuline.sun.gtk.File;
 import eu.simuline.junit.Ok;
 import eu.simuline.junit.New;
 import eu.simuline.junit.Ignored;
+import eu.simuline.junit.AssumptionFailure;
 import eu.simuline.junit.Failure;
 import eu.simuline.junit.Error;
 
 import eu.simuline.util.GifResource;
 
+import junit.framework.AssertionFailedError;
+
+import java.awt.Color;
+
 import javax.swing.ImageIcon;
+
+import org.junit.runner.notification.RunListener;// for javadoc only 
+
 
 /**
  * Represents the phases in the life-cycle of a {@link TestCase} 
  * from being {@link #Scheduled} to {@link #Ignored} 
- * or via {@link #Started} to finished (see {@link #isDecided}) 
- * which means either {@link #Success}, 
+ * or via {@link #Started} to finished 
+ * which means either {@link #Invalidated}, {@link #Success}, 
  * {@link #Failure} or even {@link #Error}. 
  * <p>
- * For each of these phases, 
- * there is a separate icon given by {@link #getIcon()}, 
- * a status message given by {@link #status()}. 
- * Also {@link #isDecided()} returns whether the outcoming of the test 
- * is decided in that phase. 
- * Methods {@link #setFinished()} and {@link #setIgnored()} 
- * specify part of the state transitions. 
+ * The phase transitions are triggered by the events 
+ * notified by a {@link RunListener} as depicted below. 
 
- *                            |
- *                            |
- *                  {@link #Scheduled}
- *                            /\
- *                           /  \
- *                          /    \
- *          {@link #Started}     {@link #Ignored} 
- *                  /\
- *                 /  \
- *                /    \
- * {@link #Success} {@link #Failure} {@link #Error}. 
+ * <pre>
+ *                                     |testRunStarted
+ *                                     |
+ *                          {@link #Scheduled}
+ *                                    /|
+ *                                   / |
+ * {@link RunListener
+ *#testIgnored(Description)}          /  |{@link RunListener#testStarted(Description)}
+ *                                 /   |
+ *                                /    |
+ *                 {@link #Ignored}    |
+ *                                     |
+ *                            {@link #Started}
+ *                                    /|\
+ * {@link RunListener#testFinished(Description)}/ | \{@link RunListener#testAssumptionFailure(Failure)}
+ *                                  /  |  \
+ *                                 /   |   \ 
+ *        ________________________/    |    \_________
+ *       /                             |              \ 
+ *      / {@link RunListener#testFailure(Failure)}|               \ 
+ *     /                              /\                \
+ *    /                              /  \                \ 
+ *   /                              /    \                \
+ * {@link #Success}   {@link #Failure} {@link #Error} {@link #Invalidated}. 
+ * </pre>
+ *
+ * Accordingly, 
+ * there are methods <code>setXXX</code> for transition to the next state: 
+ * {@link #setFinished()}, {@link #setAssumptionFailure()} and 
+ * {@link #setFailure(org.junit.runner.notification.Failure)}. 
+ * 
+ * <p>
+ * For each of these phases, there are methods to represent the quality 
+ * or further aspects of a testcase graphically: Each Quality defines 
+ * <ul>
+ * <li>a separate icon given by {@link #getIcon()}, 
+ * <li>a status message given by {@link #status()} 
+ * <li>a color {@link #getColor()} influencing the color of the progress bar 
+ * <li>a method {@link #time(long)} to compute the time required for a test run.
+ * </ul>
+ *
+ * Finally, there are further methods to ask for certain properties 
+ * as {@link #isIrregular()}, {@link #isIgnored()} and {@link #allowsRerun()}. 
  *
  * Created: Wed Jun 12 16:41:14 2006
  *
@@ -59,7 +94,7 @@ enum Quality {
      * @see #Started
      * @see #Ignored
      */
-    Scheduled {
+    Scheduled(0) {
 	ImageIcon getIcon() {
 	    return GifResource.getIcon(File.class);
 	}
@@ -75,7 +110,10 @@ enum Quality {
 	String status() {
 	    throw new UnsupportedOperationException();
 	}
-	boolean isDecided() {
+	long time(long time) {
+	    return -1;
+	}
+	boolean allowsRerun() {
 	    return false;
 	}
     },
@@ -86,7 +124,7 @@ enum Quality {
      * Thus it is neither clear whether the execution succeeds 
      * nor the outcoming of the test. 
      */
-    Started {
+    Started(0) {
 	ImageIcon getIcon() {
 	    return GifResource.getIcon(New.class);
 	}
@@ -99,17 +137,28 @@ enum Quality {
 	String status() {
 	    return "started";
 	}
-	boolean isDecided() {
+	long time(long time) {
+	    return System.currentTimeMillis();
+	}
+	boolean allowsRerun() {
 	    return false;
+	}
+	Quality setAssumptionFailure() {
+	    return Invalidated;
+	}
+ 	Quality setFailure(org.junit.runner.notification.Failure failure) {
+	    Throwable thrw = failure.getException();
+	    return thrw instanceof AssertionFailedError ? Failure : Error;
 	}
     },
     /**
-     * The execution of the testcase ran onto an hurt assumtion 
+     * The execution of the testcase ran onto an hurt assumption 
+     * before maybe running into further exceptions or errors 
      * and is thus invalidated. 
      */
-    Invalidated {
+    Invalidated(1) {
 	ImageIcon getIcon() {
-	    throw new eu.simuline.util.NotYetImplementedException();
+	    return GifResource.getIcon(AssumptionFailure.class);
 	}
 	Quality setFinished() {
 	    return this;
@@ -117,15 +166,15 @@ enum Quality {
 	String status() {
 	    return "invalidated by failed assumption";
 	}
-	boolean isDecided() {
-	    return false;
+	boolean isIgnored() {
+	    return true;
 	}
     },
     /**
      * The execution of the testcase finished and the test succeeded (passed): 
      * All assertions hold and no throwable has been thrown. 
      */
-    Success {
+    Success(0) {
 	ImageIcon getIcon() {
 	    return GifResource.getIcon(Ok.class);
 	}
@@ -142,9 +191,15 @@ enum Quality {
      * but then decided not to start execution. 
      * In particular, nothing can be said about the course of the test run. 
      */
-    Ignored {
+    Ignored(1) {
 	ImageIcon getIcon() {
 	    return GifResource.getIcon(Ignored.class);
+	}
+	long time(long time) {
+	    return 0;
+	}
+	boolean isIgnored() {
+	    return true;
 	}
 	Quality setFinished() {
 	    throw new IllegalStateException
@@ -161,12 +216,15 @@ enum Quality {
      * indicated by an {@link AssertionFailedError}. 
      * This excludes further throwables. 
      */
-    Failure {
+    Failure(2) {
 	ImageIcon getIcon() {
 	    return GifResource.getIcon(Failure.class);
 	}
 	String status() {
 	    return "failed";
+	}
+	boolean isIrregular() {
+	    return true;
 	}
     }, 
     /**
@@ -175,28 +233,84 @@ enum Quality {
      * other than {@link AssertionFailedError}. 
      * Thus there is no valid test result. 
      */
-    Error {
+    Error(2) {
 	ImageIcon getIcon() {
 	    return GifResource.getIcon(Error.class);
 	}
 	String status() {
 	    return "had an error";
 	}
+	boolean isIrregular() {
+	    return true;
+	}
     };
+
+
+    /* -------------------------------------------------------------------- *
+     * fields.                                                              *
+     * -------------------------------------------------------------------- */
+
+    /**
+     * Encoding how good the result of the test is: 
+     * <ul>
+     * <li><code>2</code> if something went wrong: 
+     *     {@link #Error} or {@link #Failure}.  
+     * <li><code>1</code> if something was ignored 
+     *     {@link #Ignored} or {@link #Invalidated}. 
+     * <li><code>0</code> else: 
+     *     {@link #Scheduled}, {@link #Started} or {@link #Success}. 
+     * </ul>
+     * This is used to define the color of the progress bar. 
+     *
+     * @see #max(Quality)
+     */
+    int level;
+
+    /* -------------------------------------------------------------------- *
+     * constructors.                                                        *
+     * -------------------------------------------------------------------- */
+
+    /**
+     * Creates another Quality with the given level {@link #level}. 
+     */
+    Quality(int level) {
+	this.level = level;
+    }
+
+ 
+    /* -------------------------------------------------------------------- *
+     * class constants.                                                     *
+     * -------------------------------------------------------------------- */
+
+    /**
+     * Represents a failed testcase. 
+     */
+    private static final Color COLOR_FAIL    = Color.red;
+
+    /**
+     * Represents a testcase which did neither failed. 
+     */
+    private static final Color COLOR_OK      = Color.green;
+
+    /**
+     * Represents an ignored testcase. 
+     */
+    private static final Color COLOR_IGNORED = Color.yellow;
 
     /* -------------------------------------------------------------------- *
      * methods.                                                             *
      * -------------------------------------------------------------------- */
 
     /**
-     * Returns an icon representing this phase on the GUI. 
-     */
-    abstract ImageIcon getIcon();
-
-    /**
-     * Returns <code>this</code> for {@link #Failure} and {@link #Error} and 
-     * returns {@link #Success} for {@link #Started}. 
+     * Returns the next phase 
+     * when {@link RunListener#testFinished(Description)} is invoked. 
      *
+     * @return
+     *    <ul>
+     *    <li><code>this</code> 
+     *       for {@link #Failure},  {@link #Invalidated} and {@link #Error}  
+     *    <li>{@link #Success} for {@link #Started}. 
+     *    </ul>
      * @throws IllegalStateException 
      *    for {@link #Scheduled}, {@link #Success} and {@link #Ignored}. 
      */
@@ -205,15 +319,40 @@ enum Quality {
     }
 
     /**
-     * Returns {@link #Ignored} for state {@link #Started}; 
-     * otherwise throws an exception. 
+     * Returns  the next phase 
+     * when {@link RunListener#testAssumptionFailure(Failure)} is invoked. 
      *
+     * @return
+     *    {@link #Invalidated} for {@link #Started}. 
      * @throws IllegalStateException 
-     *    except for {@link #Started}. 
+     *    for all but {@link #Started}. 
      */
-    Quality setIgnored() {
-	throw new IllegalStateException(this + " may not be ignored. ");
+    Quality setAssumptionFailure() {
+	throw new IllegalStateException
+	    ("Found testcase with assumtion failure which is not started. ");
     }
+
+    /**
+     * Returns  the next phase 
+     * when {@link RunListener#testFailure(Failure)} is invoked. 
+     *
+     * @return
+     *    {@link #Failure} or {@link #Error} for {@link #Started} 
+     *    depending on whether the thrown throwable 
+     *    is an {@link AssertionFailedError}. 
+     * @throws IllegalStateException 
+     *    for all but {@link #Started}. 
+     */
+    Quality setFailure(org.junit.runner.notification.Failure failure) {
+	throw new IllegalStateException
+	    ("Found testcase with failure which is not started. ");
+    }
+
+
+    /**
+     * Returns an icon representing this phase on the GUI. 
+     */
+    abstract ImageIcon getIcon();
 
     /**
      * Returns a status message which describes this phase 
@@ -225,10 +364,91 @@ enum Quality {
     abstract String status();
 
     /**
-     * Returns <code>true</code> for {@link #Success}, {@link #Failure} 
-     * and {@link #Error}; otherwise <code>false</code>. 
+     * Determines a Quality with the maximum level 
+     * of <code>this</code> and <code>other</code>. 
+     * In conjunction with {@link #getColor()}, 
+     * this is used to determine the color of the progress bar in a JUnit GUI 
+     *
+     * @see GUIRunner.TestProgressBar#incNumRunsDone(TestCase)
      */
-    boolean isDecided() {
+    Quality max(Quality other) {
+	return (this.level > other.level) ? this : other;
+    }
+
+    /**
+     * Returns the color associated with this phase: 
+     * If this phase represents an irregular ending testcase, 
+     * {@link #COLOR_FAIL} is returned. 
+     * Else if this phase represents an ignored testcase, 
+     * {@link #COLOR_IGNORED} is returned. 
+     * Else {@link #COLOR_OK} is returned. 
+     *
+     * @see #max(Quality)
+     */
+    Color getColor() {
+	if (isIrregular()) {
+	    return COLOR_FAIL;
+	}
+	if (isIgnored()) {
+	    return COLOR_IGNORED;
+	}
+
+	return COLOR_OK;
+    }
+
+    /**
+     * Returns the difference of the current time in miliseconds 
+     *
+     * @return
+     *    <ul>
+     *    <li><code>-1</code> for {@link #Scheduled}. 
+     *    <li><code>-0</code> for {@link #Ignored}. 
+     *    <li>The current time {@link System#currentTimeMillis()} 
+     *        for {@link #Started}. 
+     *    <li>the span of time the underlying testcase took 
+     *        from start to finish. 
+     *    </ul>
+     * @param time
+     *    some time in milliseconds. 
+     *    The details depend on this: 
+     *    <ul>
+     *    <li>{@link #Scheduled}, {@link #Ignored} and {@link #Started} 
+     *        ignore that parameter. 
+     *    <li>{@link #Failure},  {@link #Invalidated}, {@link #Error} and 
+     *        {@link #Success} interprete <code>time</code> 
+     *        as the start time of the underlying testcase. 
+     *    </ul>
+     */
+    long time(long time) {
+	return System.currentTimeMillis() - time;
+    }
+
+
+
+    /**
+     * Whether the run of the underlying testcase stopped irregularly. 
+     * This is the case only for {@link #Failure} and {@link #Error}. 
+     */
+    boolean isIrregular() {
+	return false;
+    }
+
+    /**
+     * Returns whether the underlying tescase is ignored. 
+     * This is <code>false</code> 
+     * except for {@link #Ignored} and {@link #Invalidated}. 
+     */
+    boolean isIgnored() {
+	return false;
+    }
+
+    /**
+     * Returns whether the underlying tescase allows to be rerun. 
+     * This is <code>false</code> 
+     * for {@link #Scheduled} and {@link #Started} only. 
+     * **** for Scheduled, this is not ideal. 
+     */
+    boolean allowsRerun() {
 	return true;
     }
 
