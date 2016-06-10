65eddc33642e9523788d091a2c44d3ecb4fcb51d
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2016-06-10 16:56:23 +0000

    @doc 	updated and improved @new 	methods testSuiteStarted/Finished 	in method testFailure(Failure) took framework failures into account. 	added method testClassStructureLoaded 	added output to ensure that nothing thrown. @chg 	eliminated field testCaseCount now integrated into TestCases. 	eliminated method isSingular() and testCaseCount. 	singular tests are now treated as all tests and absolutely uniformly.
    	in all methods eliminated singular branch. 
    
    	in method testStarted avoided creation of TestCase which is now done in GUIRunner exclusively. 
    298-->360
    
    git-svn-id: file:///SVN_Archive/Software/trunk@10421 9b6744e0-6d27-0410-8403-e1b9a971d29b

--- src/main/java/eu/simuline/testhelpers/GUIRunListener.java
+++ src/main/java/eu/simuline/testhelpers/GUIRunListener.java
@@ -33,9 +33,6 @@ public class GUIRunListener extends TextRunListener {
 
     private TestCase testCase;
 
-    // -1 before testRunStarted and for Singular. 
-    private int testCaseCount;
-
 
     /* -------------------------------------------------------------------- *
      * constructor.                                                         *
@@ -49,18 +46,6 @@ public class GUIRunListener extends TextRunListener {
     /* -------------------------------------------------------------------- *
      * methods.                                                             *
      * -------------------------------------------------------------------- */
-
-    private boolean isSingular() {
-	return this.actions.getSingleTest() != null;
-    }
-
-    private int testCaseCount() {
-	if (isSingular()) {
-	    return -1;
-	} else {
-	    return this.testCaseCount++;
-	}
-    }
 
 
     /**
@@ -82,23 +67,10 @@ public class GUIRunListener extends TextRunListener {
 	Runnable runnable = new Runnable() {
 		public void run() {
 		    GUIRunListener.this.actions.setEnableForRun(true);//running 
-		    if (GUIRunListener.this.isSingular()) {
-			GUIRunListener.this.testCase = 
-			    GUIRunListener.this.actions.getSingleTest();//NOPMD
-		    } else {
-			GUIRunListener.this.guiRunner.testRunStarted(desc);
-			GUIRunListener.this.testCaseCount = 0;
-		    }
-
-		    //assert this.testCase.getDesc().equals(desc);
-		    
-		    // 	this.runner.setStatus("single testRunStarted(");
+		    GUIRunListener.this.guiRunner.testRunStarted(desc);
 		}
 	    };
 	SwingUtilities.invokeAndWait(runnable);
-	// if (!isSingular()) {
-	//     GUIRunListener.this.testCaseCount = 0;
-	// }
     }
 
     /**
@@ -121,19 +93,62 @@ public class GUIRunListener extends TextRunListener {
 		    GUIRunListener.this.guiRunner
 		    	.setStatus("testRunFinished(required: " + 
 		    		   result.getRunTime() + "ms. ");
-		    GUIRunListener.this.actions
-			.setEnableForRun(false);// not running 
+		    GUIRunListener.this.actions.setEnableForRun(false);//!runnin
 		}
 	    };
 	SwingUtilities.invokeAndWait(runnable);
     }
 
     /**
-     * Called when an atomic test is about to be started. 
+     * Called when a test suite is about to be started. 
+     * If this method is called for a given {@link Description}, 
+     * then {@link #testSuiteFinished(Description)} 
+     * will also be called for the same {@link Description}. 
+     * <p>
+     * Note that not all runners will call this method, so runners should 
+     * be prepared to handle {@link #testStarted(Description)} calls for tests 
+     * where there was no cooresponding {@link #testSuiteStarted()} call 
+     * for the parent {@link Description}. 
+     *
+     * @param desc
+     *    the description of the test suite that is about to be run
+     *    (generally a class name)
+     * @since 4.13
+     */
+    // api-docs inherited from class RunListener
+    public void testSuiteStarted(Description desc) throws Exception {//NOPMD
+	assert !SwingUtilities.isEventDispatchThread();
+	// output text 
+	super.testSuiteStarted(desc);
+	// **** at the moment no actions on the GUI 
+    }
+
+    /**
+     * Called when a test suite has finished, 
+     * whether the test suite succeeds or fails. 
+     * This method will not be called for a given {@link Description} 
+     * unless {@link #testSuiteStarted(Description)} was called 
+     * for the same {@link Description}. 
      *
      * @param desc 
-     *    the description of the test that is about to be run 
+     *    the description of the test suite that just ran
+     * @since 4.13
+     */
+    public void testSuiteFinished(Description desc) throws Exception {//NOPMD
+	assert !SwingUtilities.isEventDispatchThread();
+	// output text 
+	super.testSuiteFinished(desc);
+	// **** at the moment no actions on the GUI 
+     }
+
+    /**
+     * Called when an atomic test is about to be started. 
+     * An ignored test is never started. 
+     *
+     * @param desc 
+     *    the description of the test that is about to be started 
      *    (generally a class and method name)
+     * @see #testIgnored(Description)
      */
     // api-docs inherited from class RunListener
     public void testStarted(final Description desc) throws Exception {//NOPMD
@@ -145,24 +160,24 @@ public class GUIRunListener extends TextRunListener {
 	Runnable runnable = new Runnable() {
 		public void run() {
 		    GUIRunListener.this.testCase = 
-			new TestCase(desc, Quality.Started, testCaseCount());
-		    if (GUIRunListener.this.isSingular()) {
-			GUIRunListener.this.guiRunner.updateSingularStarted();
-		    } else {
 			GUIRunListener.this.guiRunner
-			    .noteTestStartedI(GUIRunListener.this.testCase);
-		    }
+			.noteTestStartedI(Quality.Started);
 		}
 	    };
 	SwingUtilities.invokeAndWait(runnable);
+System.out.println("..testStarted");
     }
 
     /**
      * Called when an atomic test has finished, 
      * whether the test succeeds or fails. 
+     * This method must be invoked after a test has been started 
+     * which was indicated by {@link #testStarted(Description)} before. 
+     * An ignored test is never finished. 
      *
      * @param desc
      *    the description of the test that just ran
+     * @see #testIgnored(Description)
      */
     public void testFinished(final Description desc) throws Exception {//NOPMD
 	assert !SwingUtilities.isEventDispatchThread();
@@ -173,22 +188,24 @@ public class GUIRunListener extends TextRunListener {
 	Runnable runnable = new Runnable() {
 		public void run() {
 		    GUIRunListener.this.testCase.setFinished();
-		    if (GUIRunListener.this.isSingular()) {
-			GUIRunListener.this.guiRunner.updateSingularFinished
-			    (GUIRunListener.this.testCase);
-
-		    } else {
-			GUIRunListener.this.guiRunner
-			    .noteReportResult(GUIRunListener.this.testCase);
-		    }
+		    GUIRunListener.this.guiRunner
+			.noteReportResult(GUIRunListener.this.testCase);
 		}
 	    };
 
 	SwingUtilities.invokeAndWait(runnable);
+System.out.println("..testFinished");
     }
 
     /**
-     * Called when an atomic test fails. 
+     * Called when an atomic test fails to execute properly 
+     * throwing a Throwable. 
+     * This method is invoked after {@link #testStarted(Description)} 
+     * and before {@link #testFinished(Description)}
+     * with the according description. 
+     * In case of a failed assumption, instead of this method 
+     * {@link #testAssumptionFailure(Failure)} is invoked 
+     * for the according test. 
      *
      * @param failure 
      *    describes the test that failed and the exception that was thrown
@@ -197,7 +214,11 @@ public class GUIRunListener extends TextRunListener {
 	assert !SwingUtilities.isEventDispatchThread();
 	// output text 
 	super.testFailure(failure);
-
+	// GUI ignores failures in the test mechanism. 
+	// **** this may be inappropriate. 
+	if (failure.getDescription().equals(Description.TEST_MECHANISM)) {
+	    return;
+	}
 	Runnable runnable = new Runnable() {
 		public void run() {
 		    GUIRunListener.this.testCase.setFailure(failure);
@@ -206,6 +227,7 @@ public class GUIRunListener extends TextRunListener {
 		}
 	    };
 	SwingUtilities.invokeAndWait(runnable);
+System.out.println("..testFailure");
     }
 
 
@@ -213,6 +235,16 @@ public class GUIRunListener extends TextRunListener {
      * Called when an atomic test flags 
      * that it assumes a condition that is false. 
      * This is treated as ignored with the description of the failure. 
+     * This method is invoked after {@link #testStarted(Description)} 
+     * and before {@link #testFinished(Description)}
+     * with the according description. 
+     * A failed assertion does not count as a failure 
+     * and so {@link #testFailure(Failure)} is not invoked 
+     * for the according test. 
+     * <p>
+     * CAUTION: Although a failed assertion is like an ignored test, 
+     * {@link #testRunFinished(Result)} does not count this as ignored test 
+     * but rather than a passed test. 
      *
      * @param failure
      *    describes the test that failed 
@@ -240,6 +272,7 @@ public class GUIRunListener extends TextRunListener {
 	} catch (InvocationTargetException e) {
 	    e.printStackTrace();
 	}
+System.out.println("..testAssumptionFailure");
     }
 
     /**
@@ -247,8 +280,12 @@ public class GUIRunListener extends TextRunListener {
      * generally because a test method is annotated 
      * with <code>@Ignored</code>. 
      * This implies 
-     * that neither {@link #testStarted} nor {@link #testFinished} 
-     * are invoked. 
+     * that neither {@link #testStarted(Description)} 
+     * nor {@link #testFinished(Description)} are invoked 
+     * for the according test. 
+     * This in turn implies that neither {@link #testFailure(Failure)} 
+     * nor {@link #testAssumptionFailure(Failure)} are invoked. 
+      * are invoked. 
      *
      * @param desc 
      *    describes the test that will not be run
@@ -261,14 +298,14 @@ public class GUIRunListener extends TextRunListener {
 	Runnable runnable = new Runnable() {
 		public void run() {
 		    GUIRunListener.this.testCase = 
-			new TestCase(desc, Quality.Ignored, testCaseCount());
-		    GUIRunListener.this.guiRunner
-			.noteTestStartedI(GUIRunListener.this.testCase);
+			GUIRunListener.this.guiRunner
+			.noteTestStartedI(Quality.Ignored);
 		    GUIRunListener.this.guiRunner
 			.noteReportResult(GUIRunListener.this.testCase);
 		}
 	    };
 	SwingUtilities.invokeAndWait(runnable);
+System.out.println("...testIgnored");
     }
 
     // homemade extension 
@@ -292,6 +329,31 @@ public class GUIRunListener extends TextRunListener {
 	} catch (InvocationTargetException e) {
 	    e.printStackTrace();
 	}
+    }
+
+    // homemade extension 
+    /**
+     * Invoked if a test class is loaded defining a testsuite 
+     * described by <code>desc</code>. 
+     */
+    public void testClassStructureLoaded(final Description desc) {
+	System.out.println("S testClassStructureLoaded(" + desc);
+	Runnable runnable = new Runnable() {
+		public void run() {
+		    GUIRunListener.this.guiRunner
+			.testClassStructureLoaded(desc);
+		    //GUIRunListener.this.guiRunner.setStatus("testlassLoaded(");
+		    //GUIRunListener.this.actions.setEnableForRun(false);
+		}
+	    };
+	try {
+	    SwingUtilities.invokeAndWait(runnable);
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	} catch (InvocationTargetException e) {
+	    e.printStackTrace();
+	}
+	System.out.println("..testClassStructureLoaded(");
     }
 }
 
