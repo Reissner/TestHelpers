338ff3ef647b1062ef16c136a9a5131f57d7bf0e
ernst <ernst@9b6744e0-6d27-0410-8403-e1b9a971d29b>
2018-04-30 08:44:08 +0000

    @new 	package documentation.
    git-svn-id: file:///SVN_Archive/Software/trunk@12749 9b6744e0-6d27-0410-8403-e1b9a971d29b

---
+++ src/main/java/eu/simuline/junit/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Except class {@link AssumptionFailure}, this package contains classes 
+ * representing icons only via {@link eu.simuline.util.GifResource}. 
+ *
+ * <code>x</code> ***** NOT YET COMPLETE. 
+ */
+package eu.simuline.junit;
