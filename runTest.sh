9973f6b7251779be9cc90917174bafcf417e1418
Ernst Reissner <ernst.reissner@simuline.eu>
2022-08-23 13:04:18 +0200

    runTests.sh: more parameters, added assertions

--- runTest.sh
+++ runTest.sh
@@ -8,4 +8,4 @@ clspath=$(cat target/classpath.txt):$HERE/target/classes:$TCLS
 
 echo "$clspath"
 #java -Dsourcepath=src/main/java:src/test/java -DchooseClasspath=target/test-classes -Denvironment=. -DsrcDir=src/main/java -classpath $clspath eu.simuline.arithmetics.left2right.XFPNumberTest
-java -Dsourcepath=./src/main/java:./src/test/java -DchooseClasspath=$TCLS -classpath $clspath $1
+java -ea -esa -Dsourcepath=./src/main/java:./src/test/java -DchooseClasspath=$TCLS -classpath $clspath $@
