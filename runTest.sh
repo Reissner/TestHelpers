c1ddedc6ee6f19dda003dcfd08456f670be3d410
Ernst Reissner <ernst.reissner@simuline.eu>
2022-09-23 10:30:20 +0200

    in runTest.sh: reformatting and added a comment.

--- runTest.sh
+++ runTest.sh
@@ -6,5 +6,12 @@ TCLS="$HERE/target/test-classes"
 echo "tstcls: $TCLS"
 clspath=$(cat target/classpath.txt):$HERE/target/classes:$TCLS
 
+# shall comprise what is given in .vscode/settings.json 
+# for this project, in that file there is too much. 
+# in addition required are DchooseClasspath, -DchooseClasspath and of course -classpath
+
 echo "$clspath"
-java -ea -esa -Dsourcepath=./src/main/java:./src/test/java -DchooseClasspath=$TCLS -classpath $clspath $@
+java -ea -esa -Xverify:all -Xmx1023m \
+  -Dsourcepath=./src/main/java:./src/test/java \
+  -DchooseClasspath=$TCLS \
+  -classpath $clspath $@
